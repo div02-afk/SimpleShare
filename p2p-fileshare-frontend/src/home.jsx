1677b73acc4f636f80cfebd60654fa9abba4bc8e
Divyam Sharma <113417008+div02-afk@users.noreply.github.com>
2024-06-26 19:19:02 +0530

    frontend home setup

--- p2p-fileshare-frontend/src/home.jsx
+++ p2p-fileshare-frontend/src/home.jsx
@@ -1,14 +1,41 @@
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 export default function Home() {
-    return (
-        <div className="home">
-        <h1>Home</h1>
-        <p>Welcome to the home page!</p>
-        <button onClick={()=>{
-            window.location.href = "/send";
-        }}>Send</button>
-        <button onClick={()=>{
-            window.location.href = "/receive";
-        }}>Receive</button>
-        </div>
-    );
+  const divStyle = ` justify-center items-center flex rounded-3xl `;
+
+  return (
+    <>
+      <div className="flex h-screen items-center justify-center align-middle w-screen overflow-hidden bg-black gap-7">
+        <motion.div
+          className={divStyle + "bg-blue-500 "}
+          whileHover={{
+            width: "60%",
+            height: "60%",
+            transform: "translateX(-200px)",
+            fontSize: "5rem",
+            transition: { duration: 1, type: "spring" },
+          }}
+        >
+          <Link to="/send">
+            <motion.div className="p-10 select-none">Send</motion.div>
+          </Link>
+        </motion.div>
+        <motion.div
+          className={divStyle + "bg-red-500"}
+          whileHover={{
+            width: "60%",
+            height: "60%",
+            transform: "translateX(200px)",
+            fontSize: "5rem",
+            transition: { duration: 1, type: "spring" },
+          }}
+        >
+          <Link to="/receive">
+            <motion.div className="p-10">Receive</motion.div>
+          </Link>
+        </motion.div>
+      </div>
+    </>
+  );
 }
