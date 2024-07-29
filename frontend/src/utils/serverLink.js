f633a54e825bed67ff107429a59bad8259a23e2b
Divyam Sharma <113417008+div02-afk@users.noreply.github.com>
2024-07-29 20:35:35 +0530

    minor

--- frontend/src/utils/serverLink.js
+++ frontend/src/utils/serverLink.js
@@ -1,7 +1,7 @@
 let serverAddress = '';
 
 if(process.env.NODE_ENV === 'production'){
-     serverAddress =  'https://p2p-fileshare.onrender.com';
+     serverAddress =  'https://p2p-fileshare-server.vercel.app';
 }
 else{
     serverAddress =  'http://localhost:3000';
